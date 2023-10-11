ed8fc29f32d9ad3cccb7827a69993c72e40c0f4d
Alejandro Sanmiguel <alejandrosanmiguel0222@gmail.com>
2023-10-11 11:46:00 -0500

    Se añadieron los archivos del proyecto

---
+++ src/Basico/Arrays.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Basico;
+
+
+ //@author aleja
+ 
+public class Arrays {
+    
+    /**
+     * Arrays: Solicita al usuario 5 números, almacénalos en un array y muestra el promedio.
+     * 
+     **/
+
+    
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
