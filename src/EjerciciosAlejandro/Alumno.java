6fd6d2d9410e0d2ed919f153b0551ace29533962
Alejandro Sanmiguel <alejandrosanmiguel0222@gmail.com>
2023-10-25 19:16:37 -0500

    Se ordeno las clases y los paquetes

--- src/EjerciciosAlejandro/Alumno.java
+++ src/EjerciciosAlejandro/Alumno.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package Basico;
+package EjerciciosAlejandro;
 
 import java.util.ArrayList;
 import java.util.Scanner;
